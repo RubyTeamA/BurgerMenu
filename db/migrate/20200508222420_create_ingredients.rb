8c1265632fc0f5ff7e67dd933d13d065142ef5b2
Alexander Wong <41557633+wongalex349@users.noreply.github.com>
2020-05-11 14:30:10 -0700

    "Implemented drop down for choices and prices to everything but everything is a static dollar rn

--- db/migrate/20200508222420_create_ingredients.rb
+++ db/migrate/20200508222420_create_ingredients.rb
@@ -1,8 +1,8 @@
 class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
-      t.string :name
-      t.integer :quantity
+      t.string :name, default: "Beef Patty"
+      t.integer :quantity, default: 1
       t.boolean :completed, default: false
 
       t.timestamps null:false
